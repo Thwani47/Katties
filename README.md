928477438d7604691246e46dc868ba0dda8f7ebd
Thwani Sithole <Thwani.Sithole@derivco.co.za>
2022-05-16 22:35:48 +0200

    completed Reverse

---
+++ README.md
@@ -0,0 +1,7 @@
+## Katties Problem Archive
+
+Problems can be found [Katties](https://open.kattis.com/).
+
+---
+## Problems:
+- [Reverse](https://open.kattis.com/problems/ofugsnuid)
---
+++ Reserve/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Reserve
+{
+    public static class Reserve
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var numbers = new int[n];
+            for(var i = 0; i < n; i++)
+            {
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+            
+            for(var i = n-1 ; i >= 0 ; i--)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+        }
+    }
+}
