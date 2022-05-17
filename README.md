bc899220a5d5219fbc58a53aa06efcc1c7b1f188
Thwani Sithole <Thwani.Sithole@derivco.co.za>
2022-05-17 12:55:17 +0200

    completed SodaSlurper

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ Problems can be found [Katties](https://open.kattis.com/).
 ## Problems:
 - [Reverse](https://open.kattis.com/problems/ofugsnuid)
 - [Speed Limit](https://open.kattis.com/problems/speedlimit)
+- [Soda Slurper](https://open.kattis.com/problems/sodaslurper)
---
+++ SodaSlurper/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SodaSlurper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(' ');
+            var e = int.Parse(input[0]);
+            var f = int.Parse(input[1]);
+            var c = int.Parse(input[2]);
+            CalculateSodasDrank(e,f, c);
+        }
+
+        private static void CalculateSodasDrank(int emptyBottles, int bottlesFound, int sodaCost)
+        {
+            var totalBottles = emptyBottles + bottlesFound;
+            var sodasDrank = 0;
+            while (totalBottles >= sodaCost)
+            {
+                totalBottles -= sodaCost;
+                sodasDrank +=1;
+                totalBottles+=1;   
+            }
+            Console.WriteLine(sodasDrank);
+        }
+    }
+}
