e4e818362781a88a31f1c86c7f0995bf6e75a89a
Thwani Sithole <Thwani.Sithole@derivco.co.za>
2022-05-19 22:22:31 +0200

    completed Kemija

---
+++ Kemija/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Kemija
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            var regEx = new Regex(@"p[aeiou]{1}", RegexOptions.IgnoreCase);
+
+            input = Regex.Replace(input, @"apa", "a");
+            input = Regex.Replace(input, @"epe", "e");
+            input = Regex.Replace(input, @"ipi", "i");
+            input = Regex.Replace(input, @"opo", "o");
+            input = Regex.Replace(input, @"upu", "u");
+            Console.WriteLine(input);
+        }
+
+    }
+}
