fcd51b00f537c9d5cbeb41c45fcc96ab0bc8b7e5
Thwani Sithole <Thwani.Sithole@derivco.co.za>
2022-05-26 00:00:43 +0200

    Completed Pot

---
+++ OddEcho/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace OddEcho
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            for (var i = 1; i <= n; i++)
+            {
+                var word = Console.ReadLine();
+
+                if (i % 2 != 0)
+                {
+                    Console.WriteLine(word);
+                }
+            }
+        }
+    }
+}
